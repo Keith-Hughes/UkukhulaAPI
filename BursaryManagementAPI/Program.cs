b4b2da9a0d76f29aca17b8f1d6391a1b17cccd35
Mxolisi Sibaya <mxolisi.gojolo@gmail.com>
2024-03-03 14:44:35 +0200

    removed unused imports and changed connection string

--- BursaryManagementAPI/Program.cs
+++ BursaryManagementAPI/Program.cs
@@ -2,7 +2,7 @@
 /// The program.
 /// </summary>
 
-//"Server=tcp:ukukhulaserver.database.windows.net,1433;Initial Catalog=UkukhulaDB;Persist Security Info=False;User ID=bbdadmin;Password=password@1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;", //"Server=tcp:ukukhulaserver.database.windows.net,1433;Initial Catalog=UkukhulaDB;Persist Security Info=False;User ID=bbdadmin;Password=password@1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+
 public class Program
 {
     public static void Main(string[] args)
